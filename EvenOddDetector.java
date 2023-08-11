8698e1f3673b47f14b0d9d13a3e47322970dc0ea
thiru020604 <140637216+thiru020604@users.noreply.github.com>
2023-08-11 20:00:42 +0530

    Assignment

---
+++ EvenOddDetector.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class EvenOddDetector 
+{
+	public static void main(String[] args) 
+	{
+        	Scanner scanner = new Scanner(System.in);
+        	System.out.print("Enter a number: ");
+        	int num = scanner.nextInt();        
+        	String result = checkEvenOdd(num);
+        	System.out.println("The number " + num + " is " + result + ".");        
+        	scanner.close();
+    	}
+    	public static String checkEvenOdd(int number) 
+	{
+        	if (number % 2 == 0) 
+		{
+            		return "Even";
+        	} 
+		else 
+		{
+            		return "Odd";
+        	}
+    	}
+}
