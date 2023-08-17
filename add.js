09bf6030a6659a120f5e4531b265ab66f4b310cc
thiru020604 <140637216+thiru020604@users.noreply.github.com>
2023-08-17 15:51:49 +0530

    Assignment

---
+++ add.js
@@ -0,0 +1,9 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Example usage
+const num1 = 5;
+const num2 = 7;
+const sum = addNumbers(num1, num2);
+console.log("Sum:", sum); // Output: Sum: 12
