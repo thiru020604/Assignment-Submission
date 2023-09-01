5a175772d399f9082a542e9b92aff95305fc9701
thiru020604 <140637216+thiru020604@users.noreply.github.com>
2023-09-01 20:31:42 +0530

    expression.py

---
+++ expression.py
@@ -0,0 +1,15 @@
+# Taking user inputs for variables a and b
+a = float(input("Enter the value for variable a: "))
+b = float(input("Enter the value for variable b: "))
+
+# 1. Floor Division
+floor_division_result = a // b
+print("Floor Division Result:", floor_division_result)
+
+# 2. a power b
+power_result = a ** b
+print("a power b Result:", power_result)
+
+# 3. Check if a and b are equal
+are_equal = a == b
+print("Are a and b equal?", are_equal)
